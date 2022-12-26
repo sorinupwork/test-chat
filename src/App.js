892efd90421f4d17fc251ddaad3d82d54a9d6fd7
sorinupwork <sorin.upwork@gmail.com>
2022-12-27 01:24:43 +0200

    test-chat-react-frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Widget, addResponseMessage, addLinkSnippet } from "react-chat-widget";
+
+import "react-chat-widget/lib/styles.css";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+let messages = [];
 
 function App() {
+  const handleNewUserMessage = (newMessage) => {
+    if (newMessage.toLowerCase() === "hello") {
+      addResponseMessage(" **HELLO** ");
+    } else if (newMessage.toLowerCase() === "test") {
+      addLinkSnippet({
+        title: "Created by",
+        link: "https://sorin-adam.netlify.com/",
+        target: "_blank",
+      });
+    } else {
+      addResponseMessage("Welcome to this **awesome** chat!");
+    }
+
+    console.log(`New message incoming! ${newMessage}`);
+
+    // Now send the message throught the backend API
+    messages.push(newMessage);
+    console.log(messages);
+  };
+
+  const subtitle = <p className="subtitleText">How can we help?</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Custom messages</h1>
+        <p>hello</p>
+        <p>test</p>
+      </div>
+
+      <Widget
+        titleAvatar={logo}
+        emojis={true}
+        handleNewUserMessage={handleNewUserMessage}
+        profileAvatar={logo}
+        title={`Hi there 👋`}
+        subtitle={subtitle}
+        senderPlaceHolder={`Send us a message
+
+We tipically reply in under 3 minutes.`}
+      />
     </div>
   );
 }
